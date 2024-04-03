f18a754e2cea7a9f935d8371d1dd6a2db3de9845
bruno <brunocalegari7@gmail.com>
2024-04-03 20:28:11 -0300

    Finalizada a listagem de eventos! Iniciando a criação de eventos!

--- src/main/java/rocketseat/com/passin/services/EventService.java
+++ src/main/java/rocketseat/com/passin/services/EventService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import rocketseat.com.passin.domain.attendee.Attendee;
 import rocketseat.com.passin.domain.events.Event;
+import rocketseat.com.passin.dto.event.EventRequestDTO;
 import rocketseat.com.passin.dto.event.EventResponseDTO;
 import rocketseat.com.passin.repostories.AttendeeRepository;
 import rocketseat.com.passin.repostories.EventRepository;
@@ -21,5 +22,10 @@ public class EventService {
         Event event = this.eventRepository.findById(eventId).orElseThrow(() -> new RuntimeException("Event not found with ID:"+ eventId));
         List<Attendee> attendeesList = this.attendeeRepository.findByEventId(eventId);
         return new EventResponseDTO(event, attendeesList.size());
+    }
+
+    public void createEvent(EventRequestDTO eventDTO){
+        Event newEvent = new Event();
+        newEvent.setTitle(eventDTO.title());
     }
 }
